792e35c29c148a2354111c2fcf0024c6fdc896c8
Philip Iezzi <philip@onlime.ch>
2020-02-03 23:41:18 +0100

    added Note resource

---
+++ src/Bexio/Resource/Note.php
@@ -0,0 +1,78 @@
+<?php
+namespace Bexio\Resource;
+
+use Bexio\Bexio;
+
+/**
+ * Class Note
+ * @package Bexio\Resource
+ */
+class Note extends Bexio
+{
+   /**
+     * Gets all Notes
+     *
+     * @param array $params
+     * @return array
+     */
+    public function getNotes(array $params = [])
+    {
+        return $this->client->get('note', $params);
+    }
+
+    /**
+     * Search for notes
+     *
+     * @param array $params
+     * @return mixed
+     */
+    public function searchNotes(array $params = [])
+    {
+        return $this->client->post('note/search', $params);
+    }
+
+    /**
+     * Get specific note
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function getNote(int $id)
+    {
+        return $this->client->get("note/$id");
+    }
+
+    /**
+     * Add new note
+     * 
+     * @param array $params
+     * @return mixed
+     */
+    public function createNote(array $params = [])
+    {
+        return $this->client->post('note', $params);
+    }
+
+    /**
+     * Edit note
+     *
+     * @param $id
+     * @param array $params
+     * @return mixed
+     */
+    public function editNote(int $id, array $params = [])
+    {
+        return $this->client->post("note/$id", $params);
+    }
+
+    /**
+     * Delete specific note
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function deleteNote(int $id)
+    {
+        return $this->client->delete("note/$id");
+    }
+}
